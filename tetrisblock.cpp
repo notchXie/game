16f35d25cb2812449d9a08f508a4c9e10528ff25
xlf030606@163.com <xlf030606@163.com>
2022-08-17 00:26:56 +0800

    20220817提交0026

--- tetrisblock.cpp
+++ tetrisblock.cpp
@@ -4,3 +4,24 @@ void tetrisblock::draw(QPainter& painter)
 {
 	
 }
+
+void tetrisblock::move(int x, int y)
+{
+	if (/*按下d键*/ 1)
+	{
+		x++;
+	}
+	if (/*按下a键*/ 1)
+	{
+		x--;
+	}
+	if (/*按下s键*/ 1)
+	{
+		y--;
+	}
+	if (/*按下w键*/ 1)
+	{
+		tetrisblock::rotate();
+	}
+}
+
